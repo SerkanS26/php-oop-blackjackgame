78cbb5b5a683976ad9d5219c9162f9ce4c8e1d30
serkan <s.safran84@gmail.com>
2021-02-24 17:00:53 +0100

    game updated

--- code/index.php
+++ code/index.php
@@ -14,11 +14,57 @@ if(isset($_SESSION["blackjack"])){
     $game = $_SESSION["blackjack"];
 }else{
     $game = new Blackjack();
-//    var_dump($game);
+    $_SESSION["blackjack"] = $game;
 }
 
+echo "Player total: " . $game->getPlayer()->getScore() .PHP_EOL;
+echo "<br>";
 
-var_dump($_POST["playerAction"]);
+if (isset($_POST["playerAction"])){
+    if($_POST["playerAction"] == "hit"){
+        $game->getPlayer()->hit($game->getDeck());
+        echo "Dealer total: " . "0" .PHP_EOL;
+
+        if ($game->getPlayer()->hasLost() == true){
+           echo "<br>";
+           echo "<h2>" ."Dealer Win". "<h2>";
+        }
+    }
+}
+
+if(isset($_POST["playerAction"])){
+     if($_POST["playerAction"] == "stand"){
+         $game->getDealer()->hit($game->getDeck());
+         echo "Dealer's total: " . $game->getDealer()->getScore() .PHP_EOL;
+         echo "<br>";
+         if ($game->getDealer()->hasLost() == false){
+             if ( $game->getPlayer()->getScore() <= $game->getDealer()->getScore()){
+                 echo "<h2>" ."Dealer win!". "<h2>";
+             }
+             else{
+                 echo "<h2>" ."Player win!". "<h2>";
+             }
+         }
+         else{
+             echo "<h2>" ."Player Win". "<h2>";
+         }
+     }
+}
+
+if(isset($_POST["playerAction"])){
+    if($_POST["playerAction"] == "surrender"){
+        echo "Dealer Wins!";
+    }
+}
+//unset($_SESSION["blackjack"]);
+if(isset($_POST["playerAction"])){
+    if($_POST["playerAction"] == "restart"){
+        session_unset();
+        $_SESSION["blackjack"]= new Blackjack();
+    }
+}
+//var_dump($game->getPlayer());
+
 
 ?>
 <html>
@@ -31,11 +77,13 @@ var_dump($_POST["playerAction"]);
 </head>
 <body>
 
+
 <form action="index.php" method="post">
     <br>
     <button type="submit" name="playerAction" value="hit">Hit</button>
     <button type="submit" name="playerAction" value="surrender">Surrender</button>
     <button type="submit" name="playerAction" value="stand">Stand</button>
+    <button type="submit" name="playerAction" value="restart">Restart</button>
 </form>
 
 
